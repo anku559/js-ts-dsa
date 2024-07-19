ad069b38aed0803cac651d658fefd4303ffd1d17
anku559 <ankitsingh91@gmail.com>
2024-07-19 09:47:27 +0530

    index, d1c9304d

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
+function addTillN(n) {
+  let output = 0;
 
+  for (let i = 1; i <= n; i += 1) {
+    output += i;
+  }
+
+  return output;
+}
+
+const t1 = performance.now();
+console.log(addTillN(10000000000));
+const t2 = performance.now();
+
+console.log(`Time Elapsed: ${((t2 - t1) / 1000).toFixed(2)} seconds`);
