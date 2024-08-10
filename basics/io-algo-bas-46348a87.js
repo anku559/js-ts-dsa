6cc57e52ca11e02c620c76a5c96a7d922666395c
anku559 <ankitsingh91@gmail.com>
2024-08-10 12:51:11 +0530

    io-algo-bas-46348a87: Quadratic Equation

--- basics/io-algo-bas-46348a87.js
+++ basics/io-algo-bas-46348a87.js
@@ -7,34 +7,49 @@ Formula and Equations (Functions)
 /* Notes
  */
 
-/* Examples and Constraints
+/**
+ - ax^2 + bx + c = 0
+ - Coefficients = a, b, c
+ - Discriminant = b^2 - 4*a*c
+    - If d > 0 (positive) - two roots will be real and unequal
+    - If d === 0 - two roots will be real and equal to each other (repeated roots)
+    - If d < 0 - two roots will be complex and each root is the complex conjugate of the other root
+ */
+function rootsOfQuadraticEquation(a, b, c) {
+  if (isNaN(a) && isNaN(b) && isNaN(c)) {
+    throw new Error('Please enter valid numbers for coefficients.');
+  }
 
-====================== [Example 1] ======================
-Inputs -
-Expected Output -
+  const discriminant = b ** 2 - 4 * a * c;
 
-====================== [Example 2] ======================
-Inputs -
-Expected Output -
+  if (discriminant > 0) {
+    const root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    const root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
 
-====================== [Example 3] ======================
-Inputs - 
-Expected Output -
+    console.log('Two roots are real and unequal');
+    console.log(`Root 1: ${root1} || Root 2: ${root2}`);
+  }
 
-===================== [Constraints] =====================
-*/
+  if (discriminant === 0) {
+    const root = -b / (2 * a);
 
-function main() {}
+    console.log(
+      'Two roots will be real and equal to each other (repeated roots)',
+    );
+    console.log(`Roots: ${root}`);
+  }
+
+  if (discriminant < 0) {
+    console.log(
+      'Two roots will be complex and each root is the complex conjugate of the other root',
+    );
+  }
+}
+
+function main() {
+  rootsOfQuadraticEquation(4, -5, -12);
+}
 
 main();
 
 // Execute - node .\basics\io-algo-bas-46348a87.js
-
-/**
-Function `fnName`: `param` => `void`
-- param: int
-
-#### Worst Case Scenarios
-#### Average Case Scenarios
-#### Best Case Scenarios
-*/
